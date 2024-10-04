566348e6661b62320fa7926340c9ff9114ff8180
Matheus Heinzen <matheusheinzen7@gmail.com>
2024-10-04 15:44:40 -0300

    Correção Lógica e Dados

--- Main.java
+++ Main.java
@@ -5,68 +5,84 @@ public class Main {
         ItemBibliotecaDigital item = null;
 
         // Vídeos
-        VideoDigital rockLeeGaara = new VideoDigital("Rock Lee VS Gaara (Som de Numb)", "Kishimoto e Chester Bennington", "Melhor Luta", 2008, 441);
-        VideoDigital cartaAbertaVazio = new VideoDigital("Carta aberta para o vazio (e pra quem se perdeu nele)", "Ludo Viajante", "Reflexão", 2021, 300);
-        VideoDigital exageradoCazuza = new VideoDigital("Exagerado - Cazuza", "Cazuza", "Música", 1989, 260);
-        VideoDigital datenaMarcal = new VideoDigital("Datena agride Marçal com cadeirada durante debate da TV Cultura", "TV Cultura", "Debate", 2023, 150);
+        VideoDigital rockLeeGaara = new VideoDigital("Rock Lee VS Gaara (Som de Numb)", "Kishimoto e Chester Bennington", "Melhor Luta", 2008, 7);
+        VideoDigital cartaAbertaVazio = new VideoDigital("Carta aberta para o vazio (e pra quem se perdeu nele)", "Ludo Viajante", "Reflexão", 2021, 10);
+        VideoDigital exageradoCazuza = new VideoDigital("Exagerado - Cazuza", "Cazuza", "Música", 1989, 3);
+        VideoDigital datenaMarcal = new VideoDigital("Datena agride Marçal com cadeirada durante debate da TV Cultura", "TV Cultura", "Debate", 2023, 1);
 
         // Livros
-        EBook odisseia = new EBook("Odisseia", "Homero", "Épico", 800, 576);
-        EBook harryPotterCamaraSecreta = new EBook("Harry Potter - A Câmara Secreta", "J.K. Rowling", "Fantasia", 251, 224);
+        EBook odisseia = new EBook("Odisseia", "Homero", "Épico", 132, 576);
+        EBook harryPotterCamaraSecreta = new EBook("Harry Potter - A Câmara Secreta", "J.K. Rowling", "Fantasia", 1997, 224);
         EBook mitoDeSisifo = new EBook("O Mito de Sísifo", "Albert Camus", "Filosofia", 132, 160);
-        EBook bibliaSagrada = new EBook("Bíblia Sagrada", "Diversos Autores", "Religião", 1500, 950);
+        EBook bibliaSagrada = new EBook("Bíblia Sagrada", "Diversos Autores", "Religião", 12, 950);
 
         Scanner sc = new Scanner(System.in);
 
         while (true){
             System.out.println("[1] - Livro\n[2] - Vídeo");
-            int resposta = sc.nextInt();
+            String resposta = sc.nextLine();
 
-            if (resposta == 1) {
+            if (resposta.equals("1")) {
                 System.out.println("Temos os livros: \n[1] - Odisseia\n[2] - Harry Potter - A Câmara Secreta\n[3] - O Mito de Sísifo\n[4] - Bíblia Sagrada");
-                int escolhaLivro = sc.nextInt();
+                String escolhaLivro = sc.nextLine();
                 switch (escolhaLivro) {
-                    case 1:
+                    case "1":
                         item = odisseia;
                         break;
-                    case 2:
+                    case "2":
                         item = harryPotterCamaraSecreta;
                         break;
-                    case 3:
+                    case "3":
                         item = mitoDeSisifo;
                         break;
-                    case 4:
+                    case "4":
                         item = bibliaSagrada;
                         break;
                     default:
                         System.out.println("Opção inválida.");
                         return;
                 }
-                item.descricao();
-                item.baixar();
-            } else if (resposta == 2) {
+                System.out.println("Gostaria de ver a descrição do livro antes de o baixar?\n[1] - Sim\n[2] - Não");
+                String respostaLivro = sc.nextLine();
+                if (respostaLivro.equals("1")) {
+                    item.descricao();
+                }
+                System.out.println("Gostaria de o baixar?\n[1] - Sim\n[2] - Não");
+                String respostaBaixar = sc.nextLine();
+                if (respostaBaixar.equals("1")){
+                    item.baixar();
+                }
+            } else if (resposta.equals("2")) {
                 System.out.println("Temos os vídeos: \n[1] - Rock Lee VS Gaara (Som de Numb)\n[2] - Carta aberta para o vazio (e pra quem se perdeu nele)\n[3] - Exagerado - Cazuza\n[4] - Datena agride Marçal com cadeirada durante debate da TV Cultura");
-                int escolhaVideo = sc.nextInt();
+                String escolhaVideo = sc.nextLine();
 
                 switch (escolhaVideo) {
-                    case 1:
+                    case "1":
                         item = rockLeeGaara;
                         break;
-                    case 2:
+                    case "2":
                         item = cartaAbertaVazio;
                         break;
-                    case 3:
+                    case "3":
                         item = exageradoCazuza;
                         break;
-                    case 4:
+                    case "4":
                         item = datenaMarcal; //
                         break;
                     default:
                         System.out.println("Opção inválida.");
                         return;
                 }
-                item.descricao();
-                item.visualizar();
+                System.out.println("Gostaria de ver a descrição do vídeo antes de o visualizar?\n[1] - Sim\n[2] - Não");
+                String respostaVideo = sc.nextLine();
+                if (respostaVideo.equals("1")) {
+                    item.descricao();
+                }
+                System.out.println("Gostaria de o visualizar?\n[1] - Sim\n[2] - Não");
+                String respostaVisualizar = sc.nextLine();
+                if (respostaVisualizar.equals("1")){
+                    item.visualizar();
+                }
             } else {
                 System.out.println("Opção inválida.");
                 return;
